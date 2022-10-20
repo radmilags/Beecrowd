0dc2f1f12294e2f46012a6756bb268cdbbb4cc20
Radmila Gama <49914278+radmilags@users.noreply.github.com>
2022-10-20 10:30:40 -0300

    Create 1001 Extremamente Básico.cs

---
+++ C#/1001 Extremamente Básico.cs
@@ -0,0 +1,13 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) 
+  {
+  int a = int.Parse(Console.ReadLine());
+  int b = int.Parse(Console.ReadLine());
+  int x = a + b;
+  
+  Console.WriteLine("X = {0}", x);
+   
+  }
+}
