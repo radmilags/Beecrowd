52e1d34a6a62a07cd4a36a0c7feddeb79bdf6b5a
Radmila Gama <49914278+radmilags@users.noreply.github.com>
2022-10-20 10:26:30 -0300

    Create 3055 Nota esquecida.cs

---
+++ C#/3055 Nota esquecida.cs
@@ -0,0 +1,9 @@
+using System;
+class HelloWorld {
+  static void Main() 
+  {
+    int n = int.Parse(Console.ReadLine());
+    int m = int.Parse(Console.ReadLine());
+    Console.WriteLine(m*2-n);
+  }
+}
