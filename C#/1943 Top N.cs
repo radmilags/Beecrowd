ab3cf230717ac94537e04b9c340b576a37083b75
Radmila Gama <49914278+radmilags@users.noreply.github.com>
2022-10-20 10:25:43 -0300

    Create 1943 Top N.cs

---
+++ C#/1943 Top N.cs
@@ -0,0 +1,14 @@
+using System;
+class HelloWorld {
+  static void Main() 
+  {
+    int n = int.Parse(Console.ReadLine());
+    if(n > 50 && n <= 100) Console.WriteLine("Top 100");
+    else if(n > 25 && n <= 50) Console.WriteLine("Top 50");
+    else if(n > 10 && n <= 25) Console.WriteLine("Top 25");
+    else if(n > 5 && n <= 10) Console.WriteLine("Top 10");
+    else if(n > 3 && n <= 5) Console.WriteLine("Top 5");
+    else if(n > 1 && n <= 3) Console.WriteLine("Top 3");
+    else Console.WriteLine("Top 1");
+  }
+}
