e96bbc0dc52fc0c9ca7e3252749ff706fb0ce67f
Radmila Gama <49914278+radmilags@users.noreply.github.com>
2022-11-23 09:51:23 -0300

    Create 1005 Média 1.cpp

---
+++ C++/1005 Média 1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ double a;
+    double b;
+    double s;
+    scanf("%lf", &a);
+    scanf("%lf", &b);
+    s = (a * 3.5) + (7.5 * b);
+    s = s/11;
+    printf("MEDIA = %2.5lf\n", s);
+    return 0;
+}
