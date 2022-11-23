794dab79a4e162f5ce086217dda7971d4920096f
Radmila Gama <49914278+radmilags@users.noreply.github.com>
2022-11-23 10:02:03 -0300

    Create 2936 Quantas Mandioca?.py

---
+++ Python/2936 Quantas Mandioca?.py
@@ -0,0 +1,17 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+e = int(input())
+print(a*300+b*1500+c*600+d*1000+e*150+225)
+'''
+O Curupira come 300
+
+O Boitatá come 1500
+
+O Boto come 600
+
+O Mapinguari 1000
+
+A Iara come 150
+'''
