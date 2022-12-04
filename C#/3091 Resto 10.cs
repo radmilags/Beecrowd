bd9acc7652da67bc6e3429bbdd60a0ce2a2cb84f
Radmila Gama <49914278+radmilags@users.noreply.github.com>
2022-12-03 21:58:33 -0300

    Create 3091 Resto 10.cs

---
+++ C#/3091 Resto 10.cs
@@ -0,0 +1,13 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int n = int.Parse(Console.ReadLine());
+        int n1 = int.Parse(Console.ReadLine());
+        Console.WriteLine(n%n1);
+
+    }
+
+}
