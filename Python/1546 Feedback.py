9959c2d9c4ec1229bd6ae4307f4eba8ad8d9b826
Radmila Gama <49914278+radmilags@users.noreply.github.com>
2023-01-10 08:13:33 -0300

    Create 1546 Feedback.py

---
+++ Python/1546 Feedback.py
@@ -0,0 +1,7 @@
+for i in range(int(input())):
+    for j in range(int(input())):
+        n = int(input())
+        if n == 1: print("Rolien")
+        elif n == 2: print("Naej")
+        elif n == 3: print("Elehcim")
+        else: print("Odranoel")
