517ed11d76283def9eeb883c4edcfaf473728e34
Radmila Gama <49914278+radmilags@users.noreply.github.com>
2024-09-20 12:07:45 +0000

    Feat: Solução dos problemas 1031, 1041 e 1046

---
+++ C/1046 Tempo de Jogo.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) 
+{
+  int a, b;
+  scanf("%d %d", &a, &b);
+  if(a == b) printf("O JOGO DUROU %d HORA(S)\n", 24-a+b);
+  else if(a <= b) printf("O JOGO DUROU %d HORA(S)\n", b-a);
+  else printf("O JOGO DUROU %d HORA(S)\n",24-a+b);
+  return 0;
+}
