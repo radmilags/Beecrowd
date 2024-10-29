d0eb5b5410b78a1764b3bc0cdb73ba564c0bfec8
Radmila Gama <49914278+radmilags@users.noreply.github.com>
2024-10-29 15:04:55 -0300

    Create  2756 Saída 10.cs

---
+++ C#/2756 Saída 10.cs
@@ -0,0 +1,16 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) 
+  {
+    Console.WriteLine("       A");
+    Console.WriteLine("      B B");
+    Console.WriteLine("     C   C");
+    Console.WriteLine("    D     D");
+    Console.WriteLine("   E       E");
+    Console.WriteLine("    D     D");
+    Console.WriteLine("     C   C");
+    Console.WriteLine("      B B");
+    Console.WriteLine("       A");
+  }
+}
