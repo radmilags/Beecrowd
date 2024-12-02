d91f1240277163b6b0440dbf78fcadf6866e1d66
Radmila Gama <49914278+radmilags@users.noreply.github.com>
2024-12-02 14:50:30 -0300

    Create Fazendo Pandorgas.cs

---
+++ C#/Fazendo Pandorgas.cs
@@ -0,0 +1,15 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) 
+  {
+    int n = int.Parse(Console.ReadLine());
+    for(int i = 1; i <= n; i++)
+    {
+      string[] s = Console.ReadLine().Split(' ');
+      int a = int.Parse(s[0]);
+      int b = int.Parse(s[1]);
+      Console.WriteLine($"{(a*b)/2} cm2");
+    }
+  }
+}
