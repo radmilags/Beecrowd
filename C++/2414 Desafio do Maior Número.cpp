a8a2ffe430931bc8b488a2ed8ae7ff4818220d23
Radmila Gama <49914278+radmilags@users.noreply.github.com>
2024-12-02 15:35:31 -0300

    Create 2414 Desafio do Maior Número.cpp

---
+++ C++/2414 Desafio do Maior Número.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n; cin >> n;
+    int maior  = n;
+    while(n != 0){
+        cin >> n;
+        if(n > maior) maior = n;
+    }
+    cout<< maior << endl;
+
+    return 0;
+}
