231533f9a22f19ac43dfc30a572e25e17cf313d0
Radmila Gama <49914278+radmilags@users.noreply.github.com>
2024-12-25 13:59:10 -0300

    Create  2752 Saída 6.cs

---
+++ C#/2752 Saída 6.cs
@@ -0,0 +1,15 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) 
+  {
+    Console.WriteLine ("<AMO FAZER EXERCICIO NO URI>");
+    Console.WriteLine("<    AMO FAZER EXERCICIO NO URI>");
+    Console.WriteLine("<AMO FAZER EXERCICIO >");
+    Console.WriteLine("<AMO FAZER EXERCICIO NO URI>");
+    Console.WriteLine("<AMO FAZER EXERCICIO NO URI    >");
+    Console.WriteLine("<AMO FAZER EXERCICIO NO URI>");
+    Console.WriteLine("<          AMO FAZER EXERCICIO >");
+    Console.WriteLine("<AMO FAZER EXERCICIO           >");
+  }
+}
