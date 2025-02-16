5ec7b4ce138aa39dd327d10a2bd29da64f823937
Radmila Gama <49914278+radmilags@users.noreply.github.com>
2025-02-16 17:19:33 -0300

    Create  2759 Entrada e Saída de Carácter.cs

---
+++ C#/2759 Entrada e Saída de Carácter.cs
@@ -0,0 +1,14 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) 
+  {
+    string a = Console.ReadLine();
+    string b = Console.ReadLine();
+    string c = Console.ReadLine();
+
+    Console.WriteLine($"A = {a}, B = {b}, C = {c}");
+    Console.WriteLine($"A = {b}, B = {c}, C = {a}");
+    Console.WriteLine($"A = {c}, B = {a}, C = {b}");
+  }
+}
