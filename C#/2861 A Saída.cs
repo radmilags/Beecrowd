d0601d9ab9f70aa8281774a17c6a81077757f57b
Radmila Gama <49914278+radmilags@users.noreply.github.com>
2025-02-18 14:12:27 -0300

    Create  2861 A Saída.cs

---
+++ C#/2861 A Saída.cs
@@ -0,0 +1,14 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) 
+  {
+    Console.Clear();
+    int n = int.Parse(Console.ReadLine());
+    for (int i = 1; i <= n; i++)
+    {
+      string s = Console.ReadLine();
+      Console.WriteLine("gzuz");
+    }
+  }
+}
