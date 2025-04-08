ccfef1a1f6a3e53fbb954289d514bc091435b1f7
Radmila Gama <49914278+radmilags@users.noreply.github.com>
2025-04-08 11:30:11 -0300

    Create 2581 I am Toorg.cs

---
+++ C#/2581 I am Toorg.cs
@@ -0,0 +1,14 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) 
+  {
+    int n = int.Parse(Console.ReadLine());
+
+    for (int i = 1; i <= n; i++)
+    {
+      string a = Console.ReadLine();
+      Console.WriteLine("I am Toorg!");
+    }
+  }
+}
