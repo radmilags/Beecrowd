6fe67ee203bbedd6a4a0c41ea84e2ef426365c4f
Radmila Gama <49914278+radmilags@users.noreply.github.com>
2025-04-08 11:40:02 -0300

    Create 1117 Score Validation.cs

---
+++ C#/1117 Score Validation.cs
@@ -0,0 +1,27 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) 
+  {
+    Console.Clear();
+    double a = 0; 
+    double b = 0;
+    double x = 0;
+
+    do
+    {
+      a = double.Parse(Console.ReadLine());
+      if(a < 0 || a > 10) Console.WriteLine("nota invalida");
+      else x += a;
+    } while(a < 0 || a > 10);
+    do
+    {
+      a = double.Parse(Console.ReadLine());
+      if(a < 0 || a > 10) Console.WriteLine("nota invalida");
+      else x += a;
+    } while(a < 0 || a > 10);
+
+    x /= 2.0;
+    Console.WriteLine($"media = {x}");
+  }
+}
