c60db6e7a80f0e3cda50f69fb90ed794f9d36a98
radmilags <gama.radmila@gmail.com>
2025-04-27 00:13:46 -0300

    feat: resolucao 1188 csharp

--- C#/1188 Area Inferior.cs
+++ C#/1188 Area Inferior.cs
@@ -1,17 +1,26 @@
-using System; 
+using System;
+
 class URI {
     static void Main(string[] args) { 
-
-        double [] n = new double [144];
+        double[,] n = new double[12, 12];
         double m = 0;
         string s = Console.ReadLine();
-        for(int i = 0; i < 144; i++)
-        {
-            n[i] = double.Parse(Console.ReadLine()); 
-            m += n[i];
+        
+        for(int i = 0; i < 12; i++) {
+            for (int j = 0; j < 12; j++) {
+                n[i, j] = double.Parse(Console.ReadLine());
+            }
         }
-        if(s == "S") Console.WriteLine($"{m:0.0}");
-        else Console.WriteLine($"{m/144:0.0}");
-    }
 
+        for (int i = 7; i <= 11; i++) {
+            for (int j = 12 - i; j <= i-1; j++) {
+                m += n[i, j];
+            }
+        }
+
+        if(s == "S") 
+            Console.WriteLine($"{m:0.0}");
+        else 
+            Console.WriteLine($"{m / 30:0.0}");
+    }
 }
